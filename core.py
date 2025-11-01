77931cb334b21b997a6423a43ed833acfb968e46
zhoucheng <zhoucheng133@outlook.com>
2025-11-01 20:14:22 +0800

    Revert "整理仓库"
    This reverts commit 57993f63470baa9ea2a438acf0724e5871c29dfa.

---
+++ core.py
@@ -0,0 +1,25 @@
+import os
+from pillow_heif import register_heif_opener
+from PIL import Image
+
+register_heif_opener()
+
+def convert(input_file, output_dir, quality, use_exif) -> bool:
+    heic_path = os.path.join(input_file)
+    jpeg_path = os.path.join(
+        output_dir, os.path.splitext(os.path.basename(input_file))[0] + ".jpg"
+    )
+
+    try:
+        img = Image.open(heic_path)
+
+        exif_bytes = img.info.get("exif")
+
+        if exif_bytes and use_exif:
+            img.save(jpeg_path, "JPEG", quality=quality, exif=exif_bytes)
+        else:
+            img.save(jpeg_path, "JPEG", quality=quality)
+        
+        return True
+    except Exception as _:
+        return False
